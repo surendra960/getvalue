ef9e0dab4eebb8bb4daa5b93f0588ca9bc6f8529
Surendra Kumar Meena <smeena1@es.iit.ac.in>
2024-03-12 08:26:51 +0530

    nested list and nested map function

--- src/Nestedlistnestedarray.js
+++ src/Nestedlistnestedarray.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Table } from 'react-bootstrap';
 
 export default function Nestedlistnestedarray() {
+  const users = [
+    { name: "anil",  email: "anil@test.com",   contact: 111 , address : 
+    [{Hn : "10", city :"noida", country : 'india'},
+    {Hn : "1110", city :"delhi", country : 'india'},
+    {Hn : "12", city :"bundi", country : 'india'},
+    {Hn : "41", city :"kota", country : 'india'}
+  ] },
+    { name: "sidhu", email: "shidu@test.com", contact: 993  , address : 
+    [{Hn : "10", city :"noida", country : 'india'},
+    {Hn : "1110", city :"delhi", country : 'india'},
+    {Hn : "12", city :"bundi", country : 'india'},
+    {Hn : "41", city :"kota", country : 'india'}
+  ] },
+    { name: "sam",   email: "sam@test.com",     contact: 2343 , address : 
+    [{Hn : "10", city :"noida", country : 'india'},
+    {Hn : "1110", city :"delhi", country : 'india'},
+    {Hn : "12", city :"bundi", country : 'india'},
+    {Hn : "41", city :"kota", country : 'india'}
+  ] },
+    { name: "peter", email: "peter@test.com", contact: 111 , address : 
+    [{Hn : "10", city :"noida", country : 'india'},
+    {Hn : "1110", city :"delhi", country : 'india'},
+    {Hn : "12", city :"bundi", country : 'india'},
+    {Hn : "41", city :"kota", country : 'india'}
+  ] }
+  ];
   return (
     <div>
       <h1>NestedListNestedArray</h1>
+      <Table variant='dark'striped bordered hover> 
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Contact</td>
+            <td>Adress</td>
+          </tr>
+          {
+            users.map((item,i)=>
+              <tr key={i}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.contact}</td>
+                <td>{item.address.map((data)=>
+                <tr>
+                  <td>{data.Hn}</td>
+                  <td>{data.city}</td>
+                  <td>{data.country}</td>
+                </tr>
+                )
+                  }</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </Table>
     </div>
   )
 }
